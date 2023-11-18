a901282e7c6f8909588e2d9d738abe19826b0f87
codeFabFit <fabgar14@gmail.com>
2023-11-18 14:12:25 -0500

    GET/POST/PATCH/DELETE

--- routes/planets.mjs
+++ routes/planets.mjs
@@ -3,7 +3,19 @@ import db from "../db/conn.mjs"
 import { ObjectId } from "mongodb"
 const router = express.Router()
 
+// GET ROUTES 
 
+
+// GET - Show - get one Planet 
+
+
+// POST - create a Planet 
+
+
+//Updated - aka patch - a Planet 
+
+
+// DELETE - delete a Planet
 
 
 export default router;
