dcda165cd8c2d8faf18100c9a03bc68e651482e5
codeFabFit <fabgar14@gmail.com>
2023-11-18 21:18:48 -0500

    send request works for GET planet

--- index.mjs
+++ index.mjs
@@ -3,6 +3,8 @@ const app = express()
 const port = 3000
 import planets from './routes/planets.mjs'
 
+console.log("we did it")
+
 // Middleware 
 app.use(express.json())
 app.use('/planets', planets)
--- routes/planets.mjs
+++ routes/planets.mjs
@@ -6,14 +6,32 @@ const router = express.Router()
 // GET ROUTES 
 router.get("/", async (req, res)=> {
     const collection = await db.collection("planets") // the collection name 
-    const result = await collection.find({}).limit(8).toArray // this will find the planets and show how many there are 
+    const results = await collection.find({}).limit(8).toArray // this will find the planets and show how many there are 
 })
 
 // GET - Show - get one Planet 
+router.get('/:id', async(req, res) => {
+    const collection = await db.collection("planets")
+    const query = {_id: new ObjectId(req.params.id)}
+    const results = await collection.findOne(query)
 
+    if (!results) res.send("Not Found").status(404);
+    else res.send(results).status(200)
+})
 
 // POST - create a Planet 
+router.post('/planets/', async (req,res) => {
+    try {
+        const {name, description} = req.body;
+        const planetsConnection = db.collection('planets')
+        const result = await planetsConnection.insertOne('planets')
 
+        res.status(201).json(result.ops[0])
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server Error")
+    }
+})
 
 //Updated - aka patch - a Planet 
 
